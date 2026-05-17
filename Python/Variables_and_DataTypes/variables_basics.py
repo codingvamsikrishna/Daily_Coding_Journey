476da4a27720b15d73ceb853e80e936b0dbf58b4
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-17 21:36:45 +0530

    Add basic variables and data types example

---
+++ Python/Variables_and_DataTypes/variables_basics.py
@@ -0,0 +1,11 @@
+# Python Variables and Data Types
+
+name = "Vamsi"
+age = 23
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student Status:", is_student)
