0f4536c480068e24ef467766ff446bc89f861de3
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-19 13:20:00 +0530

    Add elif statement example for grading system

---
+++ Python/Conditional_Statements/elif_statement.py
@@ -0,0 +1,12 @@
+# Elif Statement Example
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
