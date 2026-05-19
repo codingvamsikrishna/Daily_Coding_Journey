0b1b3332166ddf105896ab598c35c4fad4fb6322
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-19 13:25:25 +0530

    Create positive_negative_number.py

---
+++ Python/Conditional_Statements/positive_negative_number.py
@@ -0,0 +1,10 @@
+# Positive or Negative Number
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive Number")
+elif number < 0:
+    print("Negative Number")
+else:
+    print("Zero")
