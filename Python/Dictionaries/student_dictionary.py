80725f57655c1b50b367a199bd8e8e7de71c8c1a
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-24 20:35:56 +0530

    Add student dictionary and print information

---
+++ Python/Dictionaries/student_dictionary.py
@@ -0,0 +1,11 @@
+# Student Information Program
+
+student = {
+    "name": "Vamsi",
+    "department": "MCA",
+    "college": "NEC"
+}
+
+print("Name:", student["name"])
+print("Department:", student["department"])
+print("College:", student["college"])
