3888598dbb8c7943d4a4c4e2a8c29fb252ec85f5
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-24 20:34:34 +0530

    Create nested_dictionary.py

---
+++ Python/Dictionaries/nested_dictionary.py
@@ -0,0 +1,15 @@
+# Nested Dictionary Example
+
+students = {
+    "student1": {
+        "name": "Vamsi",
+        "age": 23
+    },
+
+    "student2": {
+        "name": "Ram",
+        "age": 22
+    }
+}
+
+print(students["student1"]["name"])
