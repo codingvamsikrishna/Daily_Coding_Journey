5348c393fdce958f80ccb5217c543d93b7910ef1
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-05-29 21:35:43 +0530

    Create constructor_example.py

---
+++ Python/OOP/constructor_example.py
@@ -0,0 +1,13 @@
+# Constructor Example
+
+class Student:
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+student1 = Student("Vamsi", 23)
+
+print(student1.name)
+print(student1.age)
