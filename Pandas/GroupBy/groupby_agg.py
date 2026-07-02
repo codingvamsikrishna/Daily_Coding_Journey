944fefb0fea014ac648d0b1e363e95d753982ff7
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-07-02 10:41:21 +0530

    Add groupby aggregation example for employees data

---
+++ Pandas/GroupBy/groupby_agg.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv("employees.csv")
+
+result = df.groupby("Department").agg({
+    "Salary": ["sum", "mean", "max", "min"],
+    "Experience": "mean"
+})
+
+print(result)
