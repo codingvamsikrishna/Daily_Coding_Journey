4fcaf4b5e66e6ffe15f73d6f2f6fabf8f7a46d2d
Bolisetty VamsiKrishna <vamsibolisetty6281@gmail.com>
2026-07-04 11:23:43 +0530

    Add basic merge example using Pandas

---
+++ Pandas/Merging/basic_merge.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+employees = pd.read_csv("employees.csv")
+departments = pd.read_csv("departments.csv")
+
+result = pd.merge(employees, departments, on="DepartmentID")
+
+print(result)
